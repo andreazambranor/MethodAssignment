402eba010e8fce27343ed1d658f47cd66dcd9282
andreazambranor <azambranor.23@gmail.com>
2022-12-02 03:03:40 -0500

    Add project files.

---
+++ MethodAssignmentSubmisison/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignmentSubmisison
+{
+    public class Class1
+    {
+        public int Value { get; set; }
+        public int Valueb { get; set; } 
+
+
+        public int MethodA(Class1 one)
+        {
+            return Value + Valueb;
+        }
+  
+    }
+}
---
+++ MethodAssignmentSubmisison/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodAssignmentSubmisison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 one = new Class1();
+            Console.WriteLine("You will enter two values to perform math, enter value number one: ");
+
+            one.Value = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("enter parameter number 2.");
+            one.Valueb = Convert.ToInt32(Console.ReadLine());
+             
+
+
+
+            Console.WriteLine("This will return the result of an operation with an optional parameter: " + one.MethodA(one));
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
